d8d767042f7f55e1a5a5ecfb96d7e41a9a36518d
marinos <marinosch.99981@gmail.com>
2023-05-07 18:36:17 +0200

    refactored day9

--- src/main/kotlin/mpdev/springboot/aoc2019/solutions/day09/Day09.kt
+++ src/main/kotlin/mpdev/springboot/aoc2019/solutions/day09/Day09.kt
@@ -1,20 +1,20 @@
-package mpdev.springboot.aoc2019.solutions.day05
+package mpdev.springboot.aoc2019.solutions.day09
 
 import mpdev.springboot.aoc2019.model.PuzzlePartSolution
 import mpdev.springboot.aoc2019.solutions.PuzzleSolver
-import mpdev.springboot.aoc2019.solutions.icvm.Program
 import mpdev.springboot.aoc2019.solutions.icvm.InputOutput.initInputOutput
 import mpdev.springboot.aoc2019.solutions.icvm.InputOutput.setInputValues
 import mpdev.springboot.aoc2019.solutions.icvm.InputOutput.getOutputValues
+import mpdev.springboot.aoc2019.solutions.icvm.Program
 import org.springframework.stereotype.Component
 import kotlin.concurrent.thread
 import kotlin.system.measureTimeMillis
 
 @Component
-class Day05: PuzzleSolver() {
+class Day09: PuzzleSolver() {
 
     final override fun setDay() {
-        day = 5         ////// update this when a puzzle solver for a new day is implemented
+        day = 9         ////// update this when a puzzle solver for a new day is implemented
     }
 
     init {
@@ -30,24 +30,24 @@ class Day05: PuzzleSolver() {
         setInputValues(listOf(1L))
         val program = Program(inputData[0])
         val elapsed = measureTimeMillis {
-             thread(start = true, name = "self-test-0") {    // when input/output is required the intCode must run in a separate thread
+            thread(start = true, name = "self-test-0") {    // when input/output is required the intCode must run in a separate thread
                 program.run()
             }.join()
-            result = getOutputValues().last()
         }
+        result = getOutputValues().last()
         return PuzzlePartSolution(1, result.toString(), elapsed)
     }
 
     override fun solvePart2(): PuzzlePartSolution {
+        initInputOutput()
+        setInputValues(listOf(2L))
+        val program = Program(inputData[0])
         val elapsed = measureTimeMillis {
-            initInputOutput()
-            setInputValues(listOf(5L))
-            val program = Program(inputData[0])
-            thread(start = true, name = "self-test-0") {     // when input/output is required the intCode must run in a separate thread
+            thread(start = true, name = "self-test-0") {    // when input/output is required the intCode must run in a separate thread
                 program.run()
             }.join()
-            result = getOutputValues().last()
         }
+        result = getOutputValues().last()
         return PuzzlePartSolution(2, result.toString(), elapsed)
     }
 
--- src/test/kotlin/mpdev/springboot/aoc2019/day09/Day09Test.kt
+++ src/test/kotlin/mpdev/springboot/aoc2019/day09/Day09Test.kt
@@ -1,9 +1,9 @@
 package mpdev.springboot.aoc2019.day09
 
 import mpdev.springboot.aoc2019.input.InputDataReader
-import mpdev.springboot.aoc2019.solutions.day09.Day09CpuV3
-import mpdev.springboot.aoc2019.solutions.day09.InputOutput
-import mpdev.springboot.aoc2019.solutions.day09.InputProcessor09
+import mpdev.springboot.aoc2019.solutions.day09.Day09
+import mpdev.springboot.aoc2019.solutions.icvm.InputOutput
+import mpdev.springboot.aoc2019.solutions.icvm.Program
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Order
@@ -11,14 +11,13 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
-import java.math.BigInteger
 import java.util.stream.Stream
+import kotlin.concurrent.thread
 
 class Day09Test {
 
-    private val day = 9                                     ///////// Update this for a new dayN test
-    private val inputProcessor = InputProcessor09()         ///////// Update this for a new dayN test
-    private val puzzleSolver = Day09CpuV3(inputProcessor)   ///////// Update this for a new dayN test
+    private val day = 9                     ///////// Update this for a new dayN test
+    private val puzzleSolver = Day09()      ///////// Update this for a new dayN test
     private val inputDataReader = InputDataReader("src/test/resources/inputdata/input")
     private var inputLines: List<String> = inputDataReader.read(day)
 
@@ -47,41 +46,44 @@ class Day09Test {
     @Order(3)
     fun `Sets up Program object with correct memory contents`() {
         val expected = listOf(1,9,10,3,2,3,11,0,99,30,40,50)
-        val program = inputProcessor.process(inputLines)
-        for (i in program.prog.indices)
-            assertThat(program.prog[i]).isEqualTo(expected[i])
+        val program = Program(inputLines[0])
+        for (i in expected.indices)
+            assertThat(program.getMemory(i)).isEqualTo(expected[i].toLong())
     }
 
     @ParameterizedTest
     @Order(5)
     @MethodSource("provideArgsToPart1Test")
-    fun `Solves Part 1`(input: String, inputLines: List<String>, expected: List<String>) {
-        val program  = inputProcessor.process(inputLines)
-        InputOutput.input = BigInteger(input)
-        program.run()
-        println(InputOutput.output)
-        val expectedOutput = mutableListOf<BigInteger>().also { list -> expected.forEach { list.add(BigInteger(it)) } }
-        assertThat(InputOutput.output).isEqualTo(expectedOutput)
+    fun `Solves Part 1`(input: Int, inputLine: String, expected: List<Long>) {
+        val program = Program(inputLine)
+        InputOutput.initInputOutput()
+        InputOutput.setInputValues(listOf(input.toLong()))
+        thread(start = true, name = "self-test-0") {    // when input/output is required the intCode must run in a separate thread
+            program.run()
+        }.join()
+        val result = InputOutput.getOutputValues()
+        println(result)
+        assertThat(result).isEqualTo(expected)
     }
 
     companion object {
         @JvmStatic
         fun provideArgsToPart1Test(): Stream<Arguments> = Stream.of(
-            Arguments.of("0", listOf("109,-1, 4,1, 99"), listOf("-1")),
-            Arguments.of("0", listOf("109,-1, 104,1, 99"), listOf("1")),
-            Arguments.of("0", listOf("109,-1, 204,1, 99"), listOf("109")),
-            Arguments.of("0", listOf("109,1, 9,2, 204,-6, 99"), listOf("204")),
-            Arguments.of("0", listOf("109,1, 109,9, 204,-6, 99"), listOf("204")),
-            Arguments.of("0", listOf("109,1, 209,-1, 204,-106, 99"), listOf("204")),
-            Arguments.of("852", listOf("109,1, 3,3, 204, 2,99"), listOf("852")),
-            Arguments.of("9824", listOf("109,1, 203,2, 204, 2,99"), listOf("9824")),
-            Arguments.of("10", listOf("3,100, 4,100, 99"), listOf("10")),
-            Arguments.of("10", listOf("109,1, 203,1000, 4,1001, 99"), listOf("10")),
-            Arguments.of("0", listOf("1102,34915192,34915192,7,4,7,99,0"), listOf("1219070632396864")),
-            Arguments.of("0", listOf("104,112589990684262415819478,99"), listOf("112589990684262415819478")),
+            Arguments.of(0, "109,-1, 4,1, 99", listOf<Long>(-1)),
+            Arguments.of(0, "109,-1, 104,1, 99", listOf<Long>(1)),
+            Arguments.of(0, "109,-1, 204,1, 99", listOf<Long>(109)),
+            Arguments.of(0, "109,1, 9,2, 204,-6, 99", listOf<Long>(204)),
+            Arguments.of(0, "109,1, 109,9, 204,-6, 99", listOf<Long>(204)),
+            Arguments.of(0, "109,1, 209,-1, 204,-106, 99", listOf<Long>(204)),
+            Arguments.of(852, "109,1, 3,3, 204, 2,99", listOf<Long>(852)),
+            Arguments.of(9824, "109,1, 203,2, 204, 2,99", listOf<Long>(9824)),
+            Arguments.of(10, "3,100, 4,100, 99", listOf<Long>(10)),
+            Arguments.of(10, "109,1, 203,1000, 4,1001, 99", listOf<Long>(10)),
+            Arguments.of(0, "1102,34915192,34915192,7,4,7,99,0", listOf(1219070632396864)),
+            Arguments.of(0, "104,9125899906842624158,99", listOf(9125899906842624158)),
             Arguments.of(
-                "0", listOf("109,1, 204,-1, 1001,100,1,100, 1008,100,16, 101,1006,101,0, 99"),
-                listOf("109", "1", "204", "-1", "1001", "100", "1", "100", "1008", "100", "16", "101", "1006", "101", "0", "99")
+                0, "109,1, 204,-1, 1001,100,1,100, 1008,100,16, 101,1006,101,0, 99",
+                listOf<Long>(109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99)
             ),
         )
     }
