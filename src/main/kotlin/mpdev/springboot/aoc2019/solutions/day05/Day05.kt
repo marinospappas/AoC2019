138004032aaf8364fb0d3345472b1eb662850bf5
marinos <marinosch.99981@gmail.com>
2023-05-10 15:43:54 +0200

    refactored day 5

--- src/main/kotlin/mpdev/springboot/aoc2019/solutions/day05/Day05.kt
+++ src/main/kotlin/mpdev/springboot/aoc2019/solutions/day05/Day05.kt
@@ -2,12 +2,8 @@ package mpdev.springboot.aoc2019.solutions.day05
 
 import mpdev.springboot.aoc2019.model.PuzzlePartSolution
 import mpdev.springboot.aoc2019.solutions.PuzzleSolver
-import mpdev.springboot.aoc2019.solutions.icvm.ICProgram
-import mpdev.springboot.aoc2019.solutions.icvm.InputOutput.initInputOutput
-import mpdev.springboot.aoc2019.solutions.icvm.InputOutput.setInputValues
-import mpdev.springboot.aoc2019.solutions.icvm.InputOutput.getOutputValues
+import mpdev.springboot.aoc2019.solutions.icvm.ICVM
 import org.springframework.stereotype.Component
-import kotlin.concurrent.thread
 import kotlin.system.measureTimeMillis
 
 @Component
@@ -22,34 +18,30 @@ class Day05: PuzzleSolver() {
     }
 
     @Volatile
-    var result = 0L
+    var result = 0
 
     override fun initSolver() {}
 
     override fun solvePart1(): PuzzlePartSolution {
-        initInputOutput()
-        val program = ICProgram(inputData[0])
+        log.info("solving day $day part 1")
+        val icvm = ICVM(inputData[0])
         val elapsed = measureTimeMillis {
-            val t = thread(start = true, name = "self-test-0") {    // when input/output is required the intCode must run in a separate thread
-                program.run()
-            }
-            setInputValues(listOf(1L))
-            t.join()
-            result = getOutputValues().last()
+            icvm.runProgram()
+            icvm.setProgramInput(1)
+            icvm.waitProgram()
+            result = icvm.getProgramOutput().last()
         }
         return PuzzlePartSolution(1, result.toString(), elapsed)
     }
 
     override fun solvePart2(): PuzzlePartSolution {
+        log.info("solving day $day part 2")
+        val icvm = ICVM(inputData[0])
         val elapsed = measureTimeMillis {
-            initInputOutput()
-            val program = ICProgram(inputData[0])
-            val t = thread(start = true, name = "self-test-0") {     // when input/output is required the intCode must run in a separate thread
-                program.run()
-            }
-            setInputValues(listOf(5L))
-            t.join()
-            result = getOutputValues().last()
+            icvm.runProgram()
+            icvm.setProgramInput(5)
+            icvm.waitProgram()
+            result = icvm.getProgramOutput().last()
         }
         return PuzzlePartSolution(2, result.toString(), elapsed)
     }
