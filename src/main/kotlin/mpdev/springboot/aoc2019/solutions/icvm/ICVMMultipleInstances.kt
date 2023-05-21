51760f84870acd5af2cdd962937c679b2a935839
marinos <marinosch.99981@gmail.com>
2023-05-21 12:30:36 +0200

    refactor all names from thread... to instance...

--- src/main/kotlin/mpdev/springboot/aoc2019/solutions/icvm/ICVMMultipleInstances.kt
+++ src/main/kotlin/mpdev/springboot/aoc2019/solutions/icvm/ICVMMultipleInstances.kt
@@ -4,39 +4,39 @@ import kotlinx.coroutines.Job
 import mpdev.springboot.aoc2019.solutions.icvm.ProgramState.*
 
 class ICVMMultipleInstances(private val intCodeProgramString: String,
-                            threadNamePrefix: String = DEF_PROG_INSTANCE_PREFIX,
+                            instanceNamePrefix: String = DEF_PROG_INSTANCE_PREFIX,
                             ioMode: IOMode = IOMode.DIRECT):
-    ICVM(intCodeProgramString, threadNamePrefix, ioMode) {
+    ICVM(intCodeProgramString, instanceNamePrefix, ioMode) {
 
-    fun cloneInstance(ioMode: IOMode, loop: Boolean = false, threadNamePrefix: String = DEF_PROG_INSTANCE_PREFIX) {
-        addInstance(intCodeProgramString, ioMode, loop, threadNamePrefix)
+    fun cloneInstance(ioMode: IOMode, loop: Boolean = false, instanceNamePrefix: String = DEF_PROG_INSTANCE_PREFIX) {
+        addInstance(intCodeProgramString, ioMode, loop, instanceNamePrefix)
     }
 
-    fun addInstance(intCodeProgramString: String, ioMode: IOMode, loop: Boolean = false, threadNamePrefix: String = DEF_PROG_INSTANCE_PREFIX) {
+    fun addInstance(intCodeProgramString: String, ioMode: IOMode, loop: Boolean = false, instanceNamePrefix: String = DEF_PROG_INSTANCE_PREFIX) {
         val newInstance = Program(intCodeProgramString)
-        threadTable.add(newInstance)
-        threadTable.last().threadName = "$threadNamePrefix-${threadTable.lastIndex}"
-        threadTable.last().io.setIoChannels(threadTable.lastIndex, ioMode = ioMode, loop = loop)
-        log.info("IntCode instance [{}] configured", threadTable.lastIndex)
+        instanceTable.add(newInstance)
+        instanceTable.last().instanceName = "$instanceNamePrefix-${instanceTable.lastIndex}"
+        instanceTable.last().io.setIoChannels(instanceTable.lastIndex, ioMode = ioMode, loop = loop)
+        log.info("IntCode instance [{}] configured", instanceTable.lastIndex)
     }
 
     suspend fun runInstance(instanceId: Int) {
         log.info("IntCode instance [{}] starting", instanceId)
-        runIntCodeProgram(threadTable[instanceId])
+        runIntCodeProgram(instanceTable[instanceId])
     }
 
     suspend fun setInstanceInput(data: Int, instanceId: Int) {
-        setIntCodeProgramInputLong(listOf(data.toLong()), threadTable[instanceId])
+        setIntCodeProgramInputLong(listOf(data.toLong()), instanceTable[instanceId])
     }
 
     suspend fun setInstanceInput(data: List<Int>, instanceId: Int) {
-        setIntCodeProgramInputLong(data.map { it.toLong() }, threadTable[instanceId])
+        setIntCodeProgramInputLong(data.map { it.toLong() }, instanceTable[instanceId])
     }
 
     suspend fun getInstanceOutput(instanceId: Int) =
-        getIntCodeProgramOutputLong(threadTable[instanceId]).map { it.toInt() }
+        getIntCodeProgramOutputLong(instanceTable[instanceId]).map { it.toInt() }
 
-    fun instanceIsRunning(instanceId: Int) = threadTable[instanceId].programState != COMPLETED
+    fun instanceIsRunning(instanceId: Int) = instanceTable[instanceId].programState != COMPLETED
 
     suspend fun waitInstance(instanceId: Int, job: Job) {
         waitIntCodeProgram(job)
