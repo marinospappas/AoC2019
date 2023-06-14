ce1527a990cc9435567aa9f0ffa8207af6b1dafb
marinos <marinosch.99981@gmail.com>
2023-06-14 12:43:54 +0200

    day 14 part 1 and 2

---
+++ src/main/kotlin/mpdev/springboot/aoc2019/solutions/day14/Day14.kt
@@ -0,0 +1,121 @@
+package mpdev.springboot.aoc2019.solutions.day14
+
+import mpdev.springboot.aoc2019.model.PuzzlePartSolution
+import mpdev.springboot.aoc2019.solutions.PuzzleSolver
+import mpdev.springboot.aoc2019.utils.AocException
+import org.springframework.stereotype.Component
+import kotlin.math.ceil
+import kotlin.system.measureTimeMillis
+
+@Component
+class Day14: PuzzleSolver() {
+
+    companion object {
+        const val MAX_ORE = 1_000_000_000_000L
+    }
+
+    final override fun setDay() {
+        day = 14         ////// update this when a puzzle solver for a new day is implemented
+    }
+
+    init {
+        setDay()
+    }
+
+    // key = chemical produced, data = quantity of chem produced, list of ingredients
+    var reactions = mutableMapOf<String, Pair<Long, List<Chemical>> >()
+    var leftovers = mutableMapOf<String, Long>()
+    var result = 0L
+
+    override fun initSolver() {
+        reactions = mutableMapOf()
+        leftovers = mutableMapOf()
+        inputData.forEach { inputLine ->
+            val match = Regex("""(.+) => (\d+) ([A-Z]+)""").find(inputLine)
+            try {
+                val (ingredients, quantity, name) = match!!.destructured
+                reactions[ name ] = Pair(quantity.toLong(), parseIngredients(ingredients))
+            } catch (e: Exception) {
+                throw AocException("bad input line $inputLine")
+            }
+        }
+    }
+
+    fun parseIngredients(ingredients: String): List<Chemical> {
+        val chemList = mutableListOf<Chemical>()
+        val chems = ingredients.split(",")
+        chems.forEach {
+            val match = Regex("""(\d+) ([A-Z]+)""").find(it)
+            try {
+                val (quantity, name) = match!!.destructured
+                chemList.add(Chemical(quantity.toLong(), name))
+            } catch (e: Exception) {
+                throw AocException("bad ingredients list $ingredients")
+            }
+        }
+        return chemList
+    }
+
+    override fun solvePart1(): PuzzlePartSolution {
+        val elapsed = measureTimeMillis {
+            result = reactionChain(Chemical(1, "FUEL"))
+        }
+        return PuzzlePartSolution(1, result.toString(), elapsed)
+    }
+
+    override fun solvePart2(): PuzzlePartSolution {
+        val elapsed = measureTimeMillis {
+            result = tryBinaryProduction(0L, MAX_ORE)
+        }
+        return PuzzlePartSolution(2, result.toString(), elapsed)
+    }
+
+    fun reactionChain(chemical: Chemical): Long {
+        val reaction = reactions[chemical.name] ?: throw AocException("reaction not found for product ${chemical.name}")
+        val requiredQuan = manageLeftOvers(chemical.name, chemical.quantity)
+        if (requiredQuan == 0L)
+            return 0
+        val ingredients = reaction.second
+        val reactionQuan = reaction.first
+        val quantityRatio = ceil(requiredQuan.toDouble() / reactionQuan).toInt()
+        updateLeftOvers(chemical.name, requiredQuan, quantityRatio, reactionQuan)
+        return if (ingredients.size == 1 && ingredients[0].name == "ORE")
+            quantityRatio * ingredients[0].quantity
+        else
+            ingredients.sumOf { reactionChain(Chemical(quantityRatio * it.quantity, it.name)) }
+    }
+
+    fun manageLeftOvers(name: String, reqQuan: Long): Long {
+        val leftover = leftovers[name] ?: 0L
+        return if (leftover >= reqQuan) {
+            leftovers[name] = leftover - reqQuan
+            0L
+        } else {
+            leftovers[name] = 0L
+            reqQuan - leftover
+        }
+    }
+
+    fun updateLeftOvers(name: String, reqQuan: Long, quanRatio: Int, reactionQuan: Long) {
+        val thisLeftover = quanRatio * reactionQuan - reqQuan
+        leftovers[name] = (leftovers[name] ?: 0) + thisLeftover
+    }
+
+    fun tryBinaryProduction(from: Long, to: Long): Long {
+        var start = from
+        var end = to
+        while (start <= end) {
+            val mid = (start + end) / 2
+            leftovers = mutableMapOf()
+            val oreNeeded = reactionChain(Chemical(mid, "FUEL"))
+            when (oreNeeded.compareTo(MAX_ORE)) {
+                0 -> return mid
+                1 -> end = mid - 1
+                -1 -> start = mid + 1
+            }
+        }
+        return start - 1
+    }
+}
+
+data class Chemical(val quantity: Long, val name: String)
---
+++ src/test/kotlin/mpdev/springboot/aoc2019/day14/Day14Test.kt
@@ -0,0 +1,130 @@
+package mpdev.springboot.aoc2019.day14
+
+import mpdev.springboot.aoc2019.input.InputDataReader
+import mpdev.springboot.aoc2019.solutions.day14.Day14
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class Day14Test {
+
+    private val day = 14                                     ///////// Update this for a new dayN test
+    private val puzzleSolver = Day14()                        ///////// Update this for a new dayN test
+    private val inputDataReader = InputDataReader("src/test/resources/inputdata/input")
+    private var inputLines: MutableList<String> = inputDataReader.read(day).toMutableList()
+    private val input0 = listOf(
+        "10 ORE => 10 A",
+        "1 ORE => 1 B",
+        "7 A, 1 B => 1 C",
+        "7 A, 1 C => 1 D",
+        "7 A, 1 D => 1 E",
+        "7 A, 1 E => 1 FUEL"
+    )
+    private val input1 = listOf(
+        "9 ORE => 2 A",
+        "8 ORE => 3 B",
+        "7 ORE => 5 C",
+        "3 A, 4 B => 1 AB",
+        "5 B, 7 C => 1 BC",
+        "4 C, 1 A => 1 CA",
+        "2 AB, 3 BC, 4 CA => 1 FUEL"
+    )
+    private val input2 = listOf(
+        "157 ORE => 5 NZVS",
+        "165 ORE => 6 DCFZ",
+        "44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL",
+        "12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ",
+        "179 ORE => 7 PSHF",
+        "177 ORE => 5 HKGWZ",
+        "7 DCFZ, 7 PSHF => 2 XJWVT",
+        "165 ORE => 2 GPVTF",
+        "3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"
+    )
+    private val input3 = listOf(
+        "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG",
+        "17 NVRVD, 3 JNWZP => 8 VPVL",
+        "53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL",
+        "22 VJHF, 37 MNCFX => 5 FWMGM",
+        "139 ORE => 4 NVRVD",
+        "144 ORE => 7 JNWZP",
+        "5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC",
+        "5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV",
+        "145 ORE => 6 MNCFX",
+        "1 NVRVD => 8 CXFTF",
+        "1 VJHF, 6 MNCFX => 4 RFSQX",
+        "176 ORE => 6 VJHF"
+    )
+    private val input4 = listOf(
+        "171 ORE => 8 CNZTR",
+        "7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL",
+        "114 ORE => 4 BHXH",
+        "14 VRPVC => 6 BMBT",
+        "6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL",
+        "6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT",
+        "15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW",
+        "13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW",
+        "5 BMBT => 4 WPTQ",
+        "189 ORE => 9 KTJDG",
+        "1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP",
+        "12 VRPVC, 27 CNZTR => 2 XDBXC",
+        "15 KTJDG, 12 BHXH => 5 XCVML",
+        "3 BHXH, 2 VRPVC => 7 MZWV",
+        "121 ORE => 7 VRPVC",
+        "7 XCVML => 6 RJRHP",
+        "5 BHXH, 4 VRPVC => 5 LTCX"
+    )
+    private val inputs = listOf(input0, input1, input2, input3, input4)
+
+    @BeforeEach
+    fun setup() {
+        puzzleSolver.setDay()
+        puzzleSolver.inputData = inputLines
+        puzzleSolver.initSolver()
+    }
+
+    @Test
+    @Order(1)
+    fun `Sets Day correctly`() {
+        assertThat(puzzleSolver.day).isEqualTo(day)
+    }
+
+    @Test
+    @Order(2)
+    fun `Reads and Processes Input Correctly`() {
+        puzzleSolver.reactions.forEach { println(it) }
+        assertThat(puzzleSolver.reactions.size).isEqualTo(6)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "0, 31",
+        "1, 165",
+        "2, 13312",
+        "3, 180697",
+        "4, 2210736",
+    )
+    @Order(5)
+    fun `Solves Part 1`(index: Int, expected: Long) {
+        puzzleSolver.inputData = inputs[index]
+        puzzleSolver.initSolver()
+        puzzleSolver.solvePart1()
+        assertThat(puzzleSolver.result).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "2, 82892753",
+        "3, 5586022",
+        "4, 460664",
+    )
+    @Order(6)
+    fun `Solves Part 2`(index: Int, expected: Long) {
+        puzzleSolver.inputData = inputs[index]
+        puzzleSolver.initSolver()
+        puzzleSolver.solvePart2()
+        assertThat(puzzleSolver.result).isEqualTo(expected)
+    }
+}
